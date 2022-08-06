43a3af7da3717c0e4721df4860aff938b74f3b5e
Nevilaghera <nevilaghera.1@gmail.com>
2022-08-06 09:15:55 +0530

    add some data and give animetion in cover disk.

---
+++ data.js
@@ -0,0 +1,20 @@
+let song = [
+    {
+        name: 'alon',
+        path: 'music/song 1.mp3',
+        artist: 'alan wakar',
+        cover: 'images/cover 1.png',
+    },
+    {
+        name: 'alon',
+        path: 'music/song 1.mp3',
+        artist: 'alan wakar',
+        cover: 'images/cover 1.png',
+    },
+    {
+        name: 'alon',
+        path: 'music/song 1.mp3',
+        artist: 'alan wakar',
+        cover: 'images/cover 1.png',
+    }
+]
