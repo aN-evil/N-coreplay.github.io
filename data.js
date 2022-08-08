0cc38a26343292ab03c914dec91a3ceef3d55f9b
Nevilaghera <nevilaghera.1@gmail.com>
2022-08-08 23:40:17 +0530

    edit some style

--- data.js
+++ data.js
@@ -3,6 +3,19 @@ let songs = [
         name: 'alon',
         path: 'songs/Alone.mp3',
         artist: 'alan wakar',
-        cover: 'bg2.png',
+        cover: 'img/ar/alan.jpg',
     },
+    {
+        name: 'alon',
+        path: 'songs/Alone.mp3',
+        artist: 'alan wakar',
+        cover: 'img/ar/alan.jpg',
+    },
+    {
+        name: 'alon',
+        path: 'songs/Alone.mp3',
+        artist: 'alan wakar',
+        cover: 'img/ar/alan.jpg',
+    },
+    
 ]
