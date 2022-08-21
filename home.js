7594fb5b64bfea9fef2e2c51197fb631236632ff
Nevilaghera <nevilaghera.1@gmail.com>
2022-08-21 23:42:44 +0530

    blal blal blal

--- home.js
+++ home.js
@@ -1,8 +1,11 @@
-var signUp = document.querySelector('.sign-up');
-var signUp = document.querySelector('#sign-up');
+const signUp = document.querySelector('.sign-up');
+const signUp2 = document.querySelector('#sign-up');
 const login = document.querySelector('.login');
 
 signUp.addEventListener('click', ()=> {
+    window.location.href = "sign_up.html";
+})
+signUp2.addEventListener('click', ()=> {
     window.location.href = "sign_up.html";
 })
 
