d1430eda330d4e47d32ff3970dc690ee37476d68
Nevilaghera <nevilaghera.1@gmail.com>
2022-08-21 23:35:52 +0530

    add some links

---
+++ home.js
@@ -0,0 +1,11 @@
+var signUp = document.querySelector('.sign-up');
+var signUp = document.querySelector('#sign-up');
+const login = document.querySelector('.login');
+
+signUp.addEventListener('click', ()=> {
+    window.location.href = "sign_up.html";
+})
+
+login.addEventListener('click', ()=> {
+    window.location.href = "login.html";
+})
